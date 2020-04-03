0faf9b65810e867ee4f58a4c7da32c060f111e4f
Hendrik Dockhorn <hendrik.dockhorn@inside-m2m.de>
2020-04-03 12:27:30 +0200

    test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -42,12 +42,6 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-    // var mapProp = {
-    //   center: new google.maps.LatLng(18.5793, 73.8143),
-    //   zoom: 15,
-    //   mapTypeId: google.maps.MapTypeId.ROADMAP
-    // };
-    // this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
 
   handleAnimation(anim: any) {
